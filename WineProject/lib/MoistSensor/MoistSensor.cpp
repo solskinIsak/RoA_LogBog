364b01ae2f7e46ae0abf88a83fb7d03d5fa700b3
Solskin <cph-mm712@cphbusiness.dk>
2023-10-25 14:51:45 +0200

    adds comments to moisture censor class

--- WineProject/lib/MoistSensor/MoistSensor.cpp
+++ WineProject/lib/MoistSensor/MoistSensor.cpp
@@ -1,15 +1,17 @@
 #include "Arduino.h"
 #include "MoistSensor.h"
 
-MoistSensor::MoistSensor(int pin)
-: _pin(pin), _isWet(false)
+MoistSensor::MoistSensor(int pin) // Kontruktør som tager en pin som parameter
+: _pin(pin), _isWet(false) // Initialiserer pin og _isWet
 {}
 
 void MoistSensor::update()
 {
-  int sensor_analog = analogRead(_pin);
-  float moisture_percentage = ( 100 - ( (sensor_analog/1023.00) * 100 ) );
-  if(moisture_percentage>5.00)
+  int sensor_analog = analogRead(_pin); // Læser værdien fra sensoren
+  float moisture_percentage = ( 100 - ( (sensor_analog/1023.00) * 100 ) ); // Beregner procentdelen af fugtighed som en float.
+  if(moisture_percentage>5.00) /* hvis fugtigheden er over 5% er sensoren våd (sjovt nok)
+                                  men den er sat så lavt for at undgå at vinen spilder
+                                  da det tager et øjeblik for sensoren at registrere at den er våd under ophældningen.*/
   {
     _isWet = true;
   }
